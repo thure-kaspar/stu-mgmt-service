e68550e6e9e644a3f6866c6bd7d1776b10a9aa34
LukasK-roboyor <lukas.klingebiel@web.de>
2020-01-23 21:39:56 +0100

    Created mocks for services/repositories in tests

--- src/user/test/user.controller.spec.ts
+++ src/user/test/user.controller.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../controllers/user.controller';
+import { UserService } from '../services/user.service';
+
+const mock_UserService = () => ({
+
+});
 
 describe('User Controller', () => {
-  let controller: UserController;
+	let controller: UserController;
+	let userService: UserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [UserController],
+			providers: [{ provide: UserService, useFactory: mock_UserService }]
+		}).compile();
 
-    controller = module.get<UserController>(UserController);
-  });
+		controller = module.get<UserController>(UserController);
+		userService = module.get<UserService>(UserService);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
 });
