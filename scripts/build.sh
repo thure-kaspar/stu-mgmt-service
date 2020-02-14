58ee7ae751ba6feaefe2ca3d7c8f618272284fc4
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-02-14 13:47:10 +0100

    Adds build & deploy scripts

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build
+npm install
+
+# Unit tests
+#npm run test
+
+# End to End tests
+#npm run test:e2e
+
+# test coverage
+#npm run test:cov
+
+# Deploy on demo server
+./deploy.sh
