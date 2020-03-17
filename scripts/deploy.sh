618936e5a6476a8c7b3160aaf14fe9336bd40a42
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-03-17 05:40:49 +0100

    Fixing npm install (Adds missing dash)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 <<EOF
  cd ~/StudentMgmt-Backend
  git reset --hard
  git pull
- npm install -production
+ npm install --production
  rm ~/.pm2/logs/npm-error.log
  pm2 restart 0 --wait-ready # requires project intialized with: pm2 start npm -- run start:demo
  cd ..
