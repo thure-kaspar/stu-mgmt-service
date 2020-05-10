3f7da4f3093cbd64fd8d184dc92b4283443507d4
LukasK-roboyor <lukas.klingebiel@web.de>
2020-05-10 23:32:26 +0200

    Simplify group passwords

--- test/mocks/groups.mock.ts
+++ test/mocks/groups.mock.ts
@@ -5,7 +5,7 @@ export const GROUP_1_JAVA: GroupDto = {
 	id: "b4f24e81-dfa4-4641-af80-8e34e02d9c4a",
 	courseId: COURSE_JAVA_1920.id,
 	name: "Testgroup 1",
-	password: "password123",
+	password: "123",
 	isClosed: false
 };
 
@@ -13,7 +13,7 @@ export const GROUP_2_JAVA: GroupDto = {
 	id: "73b2afad-7198-4f99-86dc-a2c46c03db2c",
 	courseId: COURSE_JAVA_1920.id,
 	name: "Testgroup 2",
-	password: "password123",
+	password: "123",
 	isClosed: true
 };
 
