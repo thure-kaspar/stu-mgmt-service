00a0789d217dd501c4f322cf1d5233e8b6e741e0
LukasK-roboyor <lukas.klingebiel@web.de>
2020-06-09 21:12:50 +0200

    Replace hard coded url

--- src/course/services/update.service.ts
+++ src/course/services/update.service.ts
@@ -4,21 +4,18 @@ import { UpdateMessage } from "../../shared/dto/update-message.dto";
 @Injectable()
 export class UpdateService { 
 
-	static instance: UpdateService;
-
 	private readonly logger = new Logger(UpdateService.name);
 
-	constructor(private http: HttpService) {
-		UpdateService.instance = this;
-	}
+	constructor(private http: HttpService) { }
 
 	/**
 	 * Sends the UpdateMessage via http-post to the URL specified by the course.
 	 */
-	send(message: UpdateMessage): void {
-		//this.logger.verbose("[disabled] Sending update to: <URL>");
-		if (message.courseId === "java-sose2020") {
-			this.http.post("http://127.0.0.1:1111/rest/update", message).toPromise();
+	send(url: string, message: UpdateMessage): void {
+		if (url && message) {
+			this.logger.verbose("Sending notification to: " + url);
+			this.http.post(url, message).toPromise()
+				.catch(err  => this.logger.error(err.message));
 		}
 	}
 
