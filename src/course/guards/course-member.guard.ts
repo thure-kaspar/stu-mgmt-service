5c5563f5b08ad0cf1ed6493e7375ae1ba803f77f
LukasK-roboyor <lukas.klingebiel@web.de>
2020-07-29 15:30:46 +0200

    Attach participant to request body in course member guard
    Allows following guards and controllers to access information about the participant, i.e. to determine permissions.

--- src/course/guards/course-member.guard.ts
+++ src/course/guards/course-member.guard.ts
@@ -1,29 +1,40 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { EntityNotFoundError } from "typeorm/error/EntityNotFoundError";
 import { UserDto } from "../../shared/dto/user.dto";
-import { InjectRepository } from "@nestjs/typeorm";
-import { UserRepository } from "../../user/repositories/user.repository";
-import { User } from "../../shared/entities/user.entity";
-import { NotACourseMemberException } from "../exceptions/custom-exceptions";
 import { CourseId } from "../entities/course.entity";
+import { NotACourseMemberException } from "../exceptions/custom-exceptions";
+import { CourseParticipantsService } from "../services/course-participants.service";
 
 /**
  * Only allows requests for users, that are members of the course.
+ * Assumes that request body has requesting `user` attached to it (done by `AuthGuard`).
+ * Attaches `participant` to the request to allow following guards or controllers to access
+ * information about this participant (i.e. `role`).
+ * 
+ * @throws `NotACourseMemberException`
  */
 @Injectable()
 export class CourseMemberGuard implements CanActivate {
 	
-	constructor(@InjectRepository(User) private userRepo: UserRepository) { }
+	constructor(private courseParticipants: CourseParticipantsService) { }
 
 	async canActivate(context: ExecutionContext): Promise<boolean> {	
 		const request = context.switchToHttp().getRequest();
 		const user: UserDto = request.user;
 		const courseId: CourseId = request.params.courseId;
 
-		if (await this.userRepo.isMemberOfCourse(user.id, courseId)) {
+		try {
+			const participant = await this.courseParticipants.getParticipant(courseId, user.id);
+			// User is participant -> Attach to request, so controller can use information (i.e. role)
+			request.participant = participant;
 			return true;
+		} catch(error) {
+			if (error instanceof EntityNotFoundError) {
+				throw new NotACourseMemberException(courseId, user.id);
+			}
 		}
 
-		throw new NotACourseMemberException();
+		return false;
 	}
 
 }
