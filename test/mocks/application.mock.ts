9049e5fc42eebd5f977439f2911a07b8113c1759
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-08 11:17:52 +0200

    Add application mock that simulates requests by a student

--- test/mocks/application.mock.ts
+++ test/mocks/application.mock.ts
@@ -5,28 +5,51 @@ import { NodemailerService } from "../../src/mailing/services/nodemailer.service
 import { DisabledMailing } from "./mailing.mock";
 import { EntityNotFoundFilter } from "../../src/shared/entity-not-found.filter";
 import { AuthGuard } from "@nestjs/passport";
-import { AuthGuardMock, CourseMemberGuardMock } from "./guards.mock";
+import { AuthGuardMock, CourseMemberGuardMock_LECTURER, CourseMemberGuardMock_STUDENT } from "./guards.mock";
 import { CourseMemberGuard } from "../../src/course/guards/course-member.guard";
+
+async function initDefaultApplication(moduleFixture: TestingModule): Promise<INestApplication> {
+	const app = moduleFixture.createNestApplication();
+	app.useGlobalFilters(new EntityNotFoundFilter());
+	app.useGlobalPipes(new ValidationPipe({ transform: true })); // Automatically transform primitive params to their type
+	await app.init();
+	return app;
+}
 
 /**
  * Creates and returns an initialized NestApplication for e2e-testing purposes.
+ * Simulates that all requests are done by an `ADMIN` / `LECTURER` account that has
+ * the required permissions.
  */
 export async function createApplication(): Promise<INestApplication> {
 	const moduleFixture: TestingModule = await Test.createTestingModule({
 		imports: [AppModule],
 	})
 		.overrideGuard(AuthGuard()).useValue(new AuthGuardMock())
-		.overrideGuard(CourseMemberGuard).useClass(CourseMemberGuardMock)
+		.overrideGuard(CourseMemberGuard).useClass(CourseMemberGuardMock_LECTURER)
 		.overrideProvider(NodemailerService).useClass(DisabledMailing)
 		.compile();
 
-	const app = moduleFixture.createNestApplication();
-	app.useGlobalFilters(new EntityNotFoundFilter());
-	app.useGlobalPipes(new ValidationPipe({ transform: true })); // Automatically transform primitive params to their type
-	await app.init();
-
-	return app;
+	return initDefaultApplication(moduleFixture);
 }
+
+/**
+ * Creates and returns an initialized NestApplication for e2e-testing purposes.
+ * Simulates that all requests are done by an `USER` / `STUDENT` account.
+ */
+export async function createApplication_STUDENT(): Promise<INestApplication> {
+	const moduleFixture: TestingModule = await Test.createTestingModule({
+		imports: [AppModule],
+	})
+		.overrideGuard(AuthGuard()).useValue(new AuthGuardMock())
+		.overrideGuard(CourseMemberGuard).useClass(CourseMemberGuardMock_STUDENT)
+		.overrideProvider(NodemailerService).useClass(DisabledMailing)
+		.compile();
+
+	return initDefaultApplication(moduleFixture);
+}
+
+
 
 /**
  * Returns the imports that are required by all TestingModules.
--- test/mocks/guards.mock.ts
+++ test/mocks/guards.mock.ts
@@ -1,14 +1,14 @@
+/* eslint-disable @typescript-eslint/class-name-casing */
 import { ExecutionContext } from "@nestjs/common";
-import { copy, convertToEntity } from "../utils/object-helper";
-import { USER_SYSTEM_ADMIN, USER_MGMT_ADMIN_JAVA_LECTURER } from "./users.mock";
-import { Participant as ParticipantEntity } from "../../src/course/entities/participant.entity";
-import { User } from "../../src/shared/entities/user.entity";
-import { CourseRole } from "../../src/shared/enums";
 import { Course as CourseEntity } from "../../src/course/entities/course.entity";
-import { COURSE_JAVA_1920 } from "./courses.mock";
+import { Participant as ParticipantEntity } from "../../src/course/entities/participant.entity";
 import { Course } from "../../src/course/models/course.model";
 import { Participant } from "../../src/course/models/participant.model";
-import { UserGroupRelation } from "../../src/course/entities/user-group-relation.entity";
+import { User } from "../../src/shared/entities/user.entity";
+import { CourseRole } from "../../src/shared/enums";
+import { convertToEntity, copy } from "../utils/object-helper";
+import { COURSE_JAVA_1920 } from "./courses.mock";
+import { USER_MGMT_ADMIN_JAVA_LECTURER, USER_SYSTEM_ADMIN, USER_STUDENT_JAVA } from "./users.mock";
 
 export class AuthGuardMock { 
 	canActivate(context: ExecutionContext): boolean {
@@ -20,7 +20,7 @@ export class AuthGuardMock {
 
 export class RoleGuardMock { canActivate(): boolean { return true; }}
 
-export class CourseMemberGuardMock { 
+export class CourseMemberGuardMock_LECTURER { 
 	canActivate(context: ExecutionContext): boolean { 
 		const request = context.switchToHttp().getRequest();
 		
@@ -29,6 +29,24 @@ export class CourseMemberGuardMock {
 			userId: USER_MGMT_ADMIN_JAVA_LECTURER.id,
 			user: convertToEntity(User, USER_MGMT_ADMIN_JAVA_LECTURER),
 			role: CourseRole.LECTURER
+		});
+		
+		request.course = new Course(course);
+		request.participant = new Participant(participant);
+
+		return true; 
+	}
+}
+
+export class CourseMemberGuardMock_STUDENT { 
+	canActivate(context: ExecutionContext): boolean { 
+		const request = context.switchToHttp().getRequest();
+		
+		const course = convertToEntity(CourseEntity, COURSE_JAVA_1920);
+		const participant = new ParticipantEntity({
+			userId: USER_STUDENT_JAVA.id,
+			user: convertToEntity(User, USER_STUDENT_JAVA),
+			role: CourseRole.STUDENT
 		});
 		
 		request.course = new Course(course);
