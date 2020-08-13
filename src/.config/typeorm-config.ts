f3c7247f997d8aab957fa4a87de573682ea4c8ac
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-13 08:21:30 +0200

    Attempt to fix deployment issue

--- src/.config/typeorm-config.ts
+++ src/.config/typeorm-config.ts
@@ -1,5 +1,24 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import * as config from "config";
+import { Course } from "../course/entities/course.entity";
+import { User } from "../shared/entities/user.entity";
+import { Group } from "../course/entities/group.entity";
+import { Participant } from "../course/entities/participant.entity";
+import { UserGroupRelation } from "../course/entities/user-group-relation.entity";
+import { Assignment } from "../course/entities/assignment.entity";
+import { Assessment } from "../course/entities/assessment.entity";
+import { AssessmentUserRelation } from "../course/entities/assessment-user-relation.entity";
+import { MailTemplate } from "../mailing/entities/mail-template.entity";
+import { CourseConfig } from "../course/entities/course-config.entity";
+import { AssignmentTemplate } from "../course/entities/assignment-template.entity";
+import { GroupSettings } from "../course/entities/group-settings.entity";
+import { PartialAssessment } from "../course/entities/partial-assessment.entity";
+import { GroupEvent } from "../course/entities/group-event.entity";
+import { AssessmentAllocation } from "../course/entities/assessment-allocation.entity";
+import { AssessmentEvent } from "../course/entities/assessment-event.entity";
+import { AdmissionCriteria } from "../course/entities/admission-criteria.entity";
+import { AssignmentRegistration } from "../course/entities/assignment-group-registration.entity";
+import { GroupRegistrationRelation } from "../course/entities/group-registration-relation.entity";
 
 const dbConfig = config.get("db");
 const loggingConfig = config.get("logger");
@@ -14,6 +33,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
 	synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 	dropSchema: dbConfig.dropSchema || false,
 	keepConnectionAlive: true, // prevents AlreadyHasActiveConnectionError, needed for testing // TODO: Check if it should be disabled in production
-	entities: [__dirname + "/../**/*.entity.{js,ts}"],
+	entities: [Course, User, Group, Participant, UserGroupRelation, Assignment, Assessment, AssessmentUserRelation, MailTemplate, AssignmentRegistration,
+		GroupRegistrationRelation, CourseConfig, AssignmentTemplate, GroupSettings, AdmissionCriteria, PartialAssessment, GroupEvent, AssessmentAllocation, AssessmentEvent],
 	logging: loggingConfig.dbErrors ? ["error"] : undefined
 };
