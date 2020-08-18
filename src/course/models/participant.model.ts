3119d82c37f1e840e0047a2047a760f571ff22c4
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-18 16:58:01 +0200

    Add group related methods

--- src/course/models/participant.model.ts
+++ src/course/models/participant.model.ts
@@ -1,8 +1,10 @@
-import { CourseRole } from "../../shared/enums";
-import { AlreadyInGroupException } from "../exceptions/custom-exceptions";
-import { Participant as ParticipantEntity } from "../entities/participant.entity";
-import { GroupId } from "../entities/group.entity";
 import { UserId } from "../../shared/entities/user.entity";
+import { CourseRole } from "../../shared/enums";
+import { GroupId } from "../entities/group.entity";
+import { Participant as ParticipantEntity } from "../entities/participant.entity";
+import { AlreadyInGroupException, NotAGroupMemberException } from "../exceptions/custom-exceptions";
+import { Group } from "./group.model";
+import { ForbiddenException } from "@nestjs/common";
 
 export class Participant {
 
@@ -43,6 +45,38 @@ export class Participant {
 			throw new AlreadyInGroupException(this.userId, this.groupId);
 		}
 		return this;
+	}
+
+	/**
+	 * Asserts that the participant is a member of the given group.
+	 */
+	isMemberOfGroup(group: Group): Participant {
+		if (group.id !== this.groupId) {
+			throw new NotAGroupMemberException(group.id, this.userId);
+		}
+		return this;
+	}
+
+	/**
+	 * Asserts that the participant is allowed to remove the given participant from his group.
+	 * Currently, it is only allowed if the given participant is the participant himself. // TODO
+	 */
+	isAllowedToRemoveGroupMember(other: Participant): Participant {
+		if (!this.equals(other)) {
+			throw new ForbiddenException("Students are not allowed to remove other group members.");
+		}
+		return this;
+	}
+
+	/**
+	 * Asserts that the participant is allowed to remove their group. // TODO
+	 */
+	isAllowedToRemoveTheirGroup(): Participant {
+		return this;
+	}
+
+	equals(other: Participant): boolean {
+		return this.id === other.id;
 	}
 
 }
