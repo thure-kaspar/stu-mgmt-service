690cfc96808494a2767928ec6d3f18d407307ab7
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-08-19 08:13:07 +0200

    Removes invalid script

--- scripts/deployAPP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Deploy via ssh
-ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 <<EOF
- cd /var/www/html2/Backend-APP
- rm -f -r *
-EOF
-scp -i ~/.ssh/id_rsa_student_mgmt_backend -r dist/student-mgmt-client/* elscha@147.172.178.30:/var/www/html2/Backend-APP
