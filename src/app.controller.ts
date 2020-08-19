823642fa54faad8877f9576df839838a49b7b7e2
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-19 12:48:53 +0200

    Add route to query server uptime

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
 
-  @Get()
-	getHello(): string {
-		return this.appService.getHello();
+	private startTime = new Date();
+
+	constructor() { }
+
+	@Get("uptime")
+	getUptime(): any {
+		return {
+			startTime: this.startTime.toLocaleString(),
+			uptime: this.timeConversion(new Date().getTime() - this.startTime.getTime())
+		};
+	}
+
+	private timeConversion(difference: any): string {
+		const seconds = (difference / 1000).toFixed(1) as any;
+		const minutes = (difference / (1000 * 60)).toFixed(1) as any;
+		const hours = (difference / (1000 * 60 * 60)).toFixed(1) as any;
+		const days = (difference / (1000 * 60 * 60 * 24)).toFixed(1) as any;
+
+		if (seconds < 60) {
+			return seconds + " Sec";
+		} else if (minutes < 60) {
+			return minutes + " Min";
+		} else if (hours < 24) {
+			return hours + " Hrs";
+		} else {
+			return days + " Days";
+		}
 	}
 }
