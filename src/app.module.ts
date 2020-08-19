f287aa74808e087b6e4de5e0902d2be3cbb023a9
LukasK-roboyor <lukas.klingebiel@web.de>
2020-08-19 12:48:20 +0200

    Remove unused service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module, Provider } from "@nestjs/common";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { CourseModule } from "./course/course.module";
-import { UserModule } from "./user/user.module";
-import { TestModule } from "./test/test.module";
-import { RequestLogger } from "./utility/request.logger";
-import { typeOrmConfig } from "./.config/typeorm-config";
-import { AuthModule } from "./auth/auth.module";
 import * as config from "config";
+import { typeOrmConfig } from "./.config/typeorm-config";
+import { AppController } from "./app.controller";
+import { AuthModule } from "./auth/auth.module";
+import { CourseModule } from "./course/course.module";
 import { MailingModule } from "./mailing/mailing.module";
+import { TestModule } from "./test/test.module";
+import { UserModule } from "./user/user.module";
+import { RequestLogger } from "./utility/request.logger";
 
 const optionalProviders = (): Provider<any>[] => {
 	const providers: Provider<any>[] = [];
@@ -30,6 +29,6 @@ const optionalProviders = (): Provider<any>[] => {
 		MailingModule
 	],
 	controllers: [AppController],
-	providers: [AppService, ...optionalProviders()],
+	providers: [...optionalProviders()],
 })
 export class AppModule { }
