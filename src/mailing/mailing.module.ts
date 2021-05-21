c94cedcde6638318e9d594e7bcc637c2bb0c6ed1
klingebiel <lukas.klingebiel@web.de>
2021-05-21 08:21:22 +0200

    fix(mailing): add missing module import

--- src/mailing/mailing.module.ts
+++ src/mailing/mailing.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AssessmentRepository } from "../assessment/repositories/assessment.repository";
 import { AuthModule } from "../auth/auth.module";
+import { CourseModule } from "../course/course.module";
 import { ParticipantRepository } from "../course/repositories/participant.repository";
 import { MailingController } from "./mailing.controller";
 import { Services } from "./services";
@@ -9,7 +10,11 @@ import { MailingService } from "./services/mailing.service";
 
 @Module({
 	controllers: [MailingController],
-	imports: [AuthModule, TypeOrmModule.forFeature([ParticipantRepository, AssessmentRepository])],
+	imports: [
+		AuthModule,
+		CourseModule,
+		TypeOrmModule.forFeature([ParticipantRepository, AssessmentRepository])
+	],
 	providers: [...Services],
 	exports: [MailingService]
 })
