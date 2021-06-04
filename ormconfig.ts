0626d084581af4be97797db00463fe840bc71ed4
klingebiel <lukas.klingebiel@web.de>
2021-06-04 12:31:04 +0200

    fix: specify entities explicitly
    Debugging db connection error.

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,27 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { join } from "path";
 import { Config } from "./src/.config/config";
+import { AssessmentAllocation } from "./src/assessment/entities/assessment-allocation.entity";
+import { AssessmentEvent } from "./src/assessment/entities/assessment-event.entity";
+import { AssessmentUserRelation } from "./src/assessment/entities/assessment-user-relation.entity";
+import { Assessment } from "./src/assessment/entities/assessment.entity";
+import { PartialAssessment } from "./src/assessment/entities/partial-assessment.entity";
+import { AdmissionCriteria } from "./src/course/entities/admission-criteria.entity";
+import { AdmissionFromPreviousSemester } from "./src/course/entities/admission-from-previous-semester.entity";
+import { AssignmentRegistration } from "./src/course/entities/assignment-group-registration.entity";
+import { AssignmentTemplate } from "./src/course/entities/assignment-template.entity";
+import { Assignment } from "./src/course/entities/assignment.entity";
+import { CourseConfig } from "./src/course/entities/course-config.entity";
+import { Course } from "./src/course/entities/course.entity";
+import { GroupEvent } from "./src/course/entities/group-event.entity";
+import { GroupRegistrationRelation } from "./src/course/entities/group-registration-relation.entity";
+import { GroupSettings } from "./src/course/entities/group-settings.entity";
+import { Group } from "./src/course/entities/group.entity";
+import { Participant } from "./src/course/entities/participant.entity";
+import { UserGroupRelation } from "./src/course/entities/user-group-relation.entity";
+import { Subscriber } from "./src/notification/subscriber/subscriber.entity";
+import { User } from "./src/shared/entities/user.entity";
+import { Submission } from "./src/submission/submission.entity";
+import { UserSettings } from "./src/user/entities/user-settings.entity";
 
 const dbConfig = Config.getDb();
 const loggingConfig = Config.getLogger();
@@ -21,7 +42,30 @@ const typeOrmConfig: TypeOrmModuleOptions = {
 	// },
 	keepConnectionAlive: true, // prevents AlreadyHasActiveConnectionError, needed for testing // TODO: Check if it should be disabled in production
 	logging: loggingConfig.dbErrors ? ["error"] : undefined,
-	entities: [join(__dirname, "**/*.entity.js")]
+	entities: [
+		Course,
+		User,
+		UserSettings,
+		Group,
+		Participant,
+		UserGroupRelation,
+		Assignment,
+		Assessment,
+		AssessmentUserRelation,
+		AssignmentRegistration,
+		GroupRegistrationRelation,
+		CourseConfig,
+		AssignmentTemplate,
+		GroupSettings,
+		AdmissionCriteria,
+		AdmissionFromPreviousSemester,
+		PartialAssessment,
+		GroupEvent,
+		AssessmentAllocation,
+		AssessmentEvent,
+		Submission,
+		Subscriber
+	]
 };
 
 export default typeOrmConfig;
