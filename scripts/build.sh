a8262f466ec8f1e0fc0cc0a4b69cb4d908317d84
klingebiel <lukas.klingebiel@web.de>
2021-08-16 18:44:16 +0200

    build: add ormconfig.ts

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,7 @@ npm install
 # Create pre-compiled distribution package
 npm run build
 rm -f Backend.tar.gz
-tar czf Backend.tar.gz dist config package.json
+tar czf Backend.tar.gz dist config package.json ormconfig.ts
 
 # Deploy on demo server
 scripts/deploy.sh
