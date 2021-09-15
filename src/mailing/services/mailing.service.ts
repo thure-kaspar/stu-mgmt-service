361923636bda0819508695110f28e25bfd68e6e7
klingebiel <lukas.klingebiel@web.de>
2021-09-15 14:09:32 +0200

    fix(mailing): avoid crashing server when mailing fails

--- src/mailing/services/mailing.service.ts
+++ src/mailing/services/mailing.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { Mail } from "../mail.model";
 import { NodemailerService } from "./nodemailer.service";
 
 @Injectable()
 export class MailingService {
+	private logger = new Logger(MailingService.name);
+
 	constructor(private nodemailer: NodemailerService) {}
 
 	async send(mail: Mail): Promise<void> {
-		return this.nodemailer.send(mail);
+		try {
+			await this.nodemailer.send(mail);
+		} catch (error) {
+			this.logger.error("Failed to send mails:");
+			console.log(error);
+		}
 	}
 }
