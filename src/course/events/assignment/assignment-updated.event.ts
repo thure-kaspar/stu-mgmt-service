961f9c7397cf6eeed2c6b88d90f545a0780752ed
klingebiel <lukas.klingebiel@web.de>
2021-11-30 14:22:37 +0100

    feat(assignment): expose AssignmentUpdated event

--- src/course/events/assignment/assignment-updated.event.ts
+++ src/course/events/assignment/assignment-updated.event.ts
@@ -1,3 +1,15 @@
-export class AssignmentUpdated {
+import { Event } from "..";
+import { NotificationDto } from "../../../shared/dto/notification.dto";
+import { INotify } from "../interfaces";
+
+export class AssignmentUpdated implements INotify {
 	constructor(readonly courseId: string, readonly assignmentId: string) {}
+
+	toNotificationDto(): NotificationDto {
+		return {
+			event: Event.ASSIGNMENT_UPDATED,
+			courseId: this.courseId,
+			assignmentId: this.assignmentId
+		};
+	}
 }
