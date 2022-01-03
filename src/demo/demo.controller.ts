d4a3197a95c24de4070a54428cd648a751393166
klingebiel <lukas.klingebiel@web.de>
2022-01-03 18:48:48 +0100

    refactor: add log statement for db reset

--- src/demo/demo.controller.ts
+++ src/demo/demo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, NotImplementedException, Post } from "@nestjs/common";
+import { Controller, Logger, NotImplementedException, Post } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { getConnection } from "typeorm";
 import { DbMockService } from "../../test/mocks/db-mock.service";
@@ -7,6 +7,8 @@ import { environment } from "../.config/environment";
 @ApiTags("demo")
 @Controller("demo")
 export class DemoController {
+	logger = new Logger(DemoController.name);
+
 	@ApiOperation({
 		operationId: "reset",
 		summary: "Reset demo database.",
@@ -18,9 +20,10 @@ export class DemoController {
 			throw new NotImplementedException("This method is disabled in production environment.");
 		}
 
+		this.logger.debug("Resetting database to initial state...");
+
 		const connection = getConnection();
 		await connection.synchronize(true); // Drop data and rebuild (empty) database
-		const dbMockService = new DbMockService(connection);
-		await dbMockService.createAll();
+		await new DbMockService(connection).createAll();
 	}
 }
