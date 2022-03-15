e9dd9bbc3a975a815122c2d5222bee5a6eb1d5b0
klingebiel <lukas.klingebiel@web.de>
2022-03-15 18:26:03 +0100

    feat(groups): merge by activity (wip)

--- src/course/services/merge-by-activity.strategy.ts
+++ src/course/services/merge-by-activity.strategy.ts
@@ -1,17 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Activity } from "../../activity/activity.entity";
+import { ActivityService } from "../../activity/activity.service";
+import { SubmissionService } from "../../submission/submission.service";
 import { ParticipantDto } from "../dto/course-participant/participant.dto";
 import { GroupDto } from "../dto/group/group.dto";
 import { ParticipantRepository } from "../repositories/participant.repository";
+import * as dayjs from "dayjs";
+import * as weekOfYear from "dayjs/plugin/weekOfYear";
+import { SubmissionDto } from "../../submission/submission.dto";
+import { ActivityDto } from "../../activity/activity.dto";
+
+dayjs.extend(weekOfYear);
 
 @Injectable()
 export class MergeByActivityStrategy {
 	constructor(
 		@InjectRepository(ParticipantRepository)
 		private participantsRepository: ParticipantRepository,
-		@InjectRepository(Activity) private activityRepository: Repository<Activity>
+		private activityService: ActivityService,
+		private submissions: SubmissionService
 	) {}
 
 	async merge(
@@ -20,36 +27,140 @@ export class MergeByActivityStrategy {
 		maxSize: number,
 		groups: { name: string; members: ParticipantDto[] }[]
 	): Promise<GroupDto[]> {
-		const nonEmptyGroups = groups.filter(g => g.members.length > 0);
+		const invalidGroups = groups.filter(
+			g => g.members.length >= 0 && g.members.length < minSize
+		);
 
-		// Groups that have multiple members, but are below minSize
-		const mustBeFilled = nonEmptyGroups
-			.filter(g => g.members.length < minSize && g.members.length > 1)
-			.sort((a, b) => a.members.length - b.members.length);
+		const studentsInInvalidGroups = invalidGroups.flatMap(g => g.members);
+		const studentsInInvalidGroupsUserIds = studentsInInvalidGroups.map(p => p.userId);
 
-		// Groups with only one member
-		const singles = nonEmptyGroups.filter(g => g.members.length == 1);
+		const [activity, [submissions]] = await Promise.all([
+			this.activityService.getActivityData(courseId),
+			this.submissions.getAllSubmissions(courseId)
+		]);
 
-		// Fill up groups first
+		const activityOfStudentsFromInvalidGroups = activity.filter(a =>
+			studentsInInvalidGroupsUserIds.includes(a.user.userId)
+		);
+
+		const activeWeekCountByStudent = countActiveWeeks(activityOfStudentsFromInvalidGroups);
+
+		const submittedAssignmentsByStudent = countSubmittedAssignment(
+			submissions,
+			studentsInInvalidGroupsUserIds
+		);
+
+		const rankedStudents = sortStudentsByActivity(
+			studentsInInvalidGroups,
+			submittedAssignmentsByStudent,
+			activeWeekCountByStudent
+		);
+
+		const idealGroupSizes = calculateIdealGroupSizes(rankedStudents.length, minSize, maxSize);
 
 		return [];
 	}
 }
 
-/**
- * - Assign average activity score to each group
- * - Try to fill up open groups with capacity (filter closed with size >= minSize)
- * - If group is too small, add student with nearest activity score
- * - If remaining students can't create valid group, backtrack from filling up and
- *
- *
- * - Only look at groups that are too small
- * - Rank them by average activity score
- * - Split by maxSize
- * - If last group is too small and minSize < maxSize, switch student down until minSize reached
- *
- * - [8, 12345, 123]
- * - [8, 1234, 1234]
- *
- * -
- */
+export function calculateIdealGroupSizes(
+	numberOfStudents: number,
+	minSize: number,
+	maxSize: number
+): number[] {
+	let idealGroupSize = -1;
+
+	for (let size = maxSize; size >= minSize; size--) {
+		const remaining = numberOfStudents % size;
+
+		if (remaining == 0 || remaining >= minSize) {
+			idealGroupSize = size;
+			// TODO
+		}
+	}
+
+	return [];
+}
+
+function countActiveWeeks(activity: ActivityDto[]) {
+	const activeWeekCountByStudent = new Map<string, number>();
+	for (const act of activity) {
+		activeWeekCountByStudent.set(act.user.userId, countUniqueWeeks(act.dates));
+	}
+	return activeWeekCountByStudent;
+}
+
+export function sortStudentsByActivity(
+	studentsInInvalidGroups: ParticipantDto[],
+	submittedAssignmentsByStudent: Map<string, number>,
+	activeWeekCountByStudent: Map<string, number>
+): ParticipantDto[] {
+	return studentsInInvalidGroups.sort((a, b) => {
+		const submittedAssignmentsA = submittedAssignmentsByStudent.get(a.userId);
+		const submittedAssignmentsB = submittedAssignmentsByStudent.get(b.userId);
+
+		const activeWeeksA = activeWeekCountByStudent.get(a.userId);
+		const activeWeeksB = activeWeekCountByStudent.get(b.userId);
+
+		if (submittedAssignmentsA === submittedAssignmentsB) {
+			return activeWeeksB - activeWeeksA;
+		} else {
+			return submittedAssignmentsB - submittedAssignmentsA;
+		}
+	});
+}
+
+export function getWeekOfYear(date: Date): number {
+	return dayjs(date).week();
+}
+
+export function isSameWeek(d1: Date, d2: Date): boolean {
+	return dayjs(d1).isSame(d2, "week");
+}
+
+export function countSubmittedAssignment(
+	submissions: SubmissionDto[],
+	userIds: string[]
+): Map<string, number> {
+	const relevantStudents = new Map<string, { [assignmentId: string]: boolean }>();
+	userIds.forEach(id => {
+		relevantStudents.set(id, {});
+	});
+
+	for (const submission of submissions) {
+		const submittedAssignments = relevantStudents.get(submission.userId);
+
+		if (submittedAssignments) {
+			submittedAssignments[submission.assignmentId] = true;
+		}
+	}
+
+	const countMap = new Map<string, number>();
+
+	relevantStudents.forEach((submittedAssignment, userId) => {
+		const count = Object.keys(submittedAssignment).length;
+		countMap.set(userId, count);
+	});
+
+	return countMap;
+}
+
+export function countUniqueWeeks(dates: Date[]): number {
+	if (dates.length <= 1) {
+		return dates.length;
+	}
+
+	let count = 1;
+	let current = 0;
+	let next = 1;
+
+	while (current < dates.length && next < dates.length) {
+		if (isSameWeek(dates[current], dates[next])) {
+			next++; // Move right index further to the right
+		} else {
+			count++;
+			current = next; // Set left index to first date in new week
+		}
+	}
+
+	return count;
+}
--- test/unit/activity/merge-by-activity.spec.ts
+++ test/unit/activity/merge-by-activity.spec.ts
@@ -6,7 +6,19 @@ import { CourseWithGroupSettings } from "../../../src/course/models/course-with-
 import { Group } from "../../../src/course/models/group.model";
 import { Participant } from "../../../src/course/models/participant.model";
 import { GroupMergeStrategy } from "../../../src/course/services/group-merge.strategy";
+import {
+	MergeByActivityStrategy,
+	isSameWeek,
+	getWeekOfYear,
+	countUniqueWeeks,
+	countSubmittedAssignment,
+	sortStudentsByActivity
+} from "../../../src/course/services/merge-by-activity.strategy";
 import { User } from "../../../src/shared/entities/user.entity";
+
+import * as dayjs from "dayjs";
+import { SubmissionDto } from "../../../src/submission/submission.dto";
+import { ParticipantDto } from "../../../src/course/dto/course-participant/participant.dto";
 
 xdescribe("GroupMergeStrategy", () => {
 	describe("Min: 2, Max: 2", () => {
@@ -173,8 +185,23 @@ xdescribe("GroupMergeStrategy", () => {
 });
 
 describe("MergeByActivityStrategy", () => {
+	//let mergeByActivityStrategy: MergeByActivityStrategy;
+
 	it("merge", async () => {
-		expect(1).toEqual(1);
+		const isSame = isSameWeek(new Date(2022, 1, 3), new Date(2022, 1, 5));
+		expect(isSame).toEqual(true);
+	});
+
+	test.each([
+		[new Date(2022, 0, 1), 1],
+		[new Date(2022, 0, 2), 2],
+		[new Date(2022, 0, 9), 3],
+		[new Date(2022, 0, 16), 4],
+		[new Date(2022, 11, 31), 53],
+		[new Date(2018, 5, 27), 26],
+		[new Date(2018, 5, 20), 25]
+	])("%s - Week: %s", (date, expected) => {
+		expect(getWeekOfYear(date)).toEqual(expected);
 	});
 });
 
@@ -190,3 +217,133 @@ function createFakeGroup(name: string, members: string[]) {
 
 	return fullGroup;
 }
+
+describe("countUniqueWeeks", () => {
+	it("0 Dates -> 0 Unique weeks", () => {
+		expect(countUniqueWeeks([])).toEqual(0);
+	});
+
+	it("1 Date -> 1 Unique week", () => {
+		expect(countUniqueWeeks([new Date(2022, 1, 1)])).toEqual(1);
+	});
+
+	it("2 Dates in same week -> 1 Unique week", () => {
+		expect(countUniqueWeeks([new Date(2022, 1, 1), new Date(2022, 1, 2)])).toEqual(1);
+	});
+
+	it("2 Dates in different weeks -> 2 Unique weeks", () => {
+		expect(countUniqueWeeks([new Date(2022, 1, 1), new Date(2022, 1, 22)])).toEqual(2);
+	});
+
+	it("5 Dates in different 3 weeks -> 3 Unique weeks", () => {
+		expect(
+			countUniqueWeeks([
+				new Date(2022, 1, 1),
+				new Date(2022, 1, 2),
+				new Date(2022, 1, 11),
+				new Date(2022, 1, 12),
+				new Date(2022, 1, 30)
+			])
+		).toEqual(3);
+	});
+});
+
+describe("countSubmittedAssignments", () => {
+	const submission = (assignmentId: string, userId: string): SubmissionDto => {
+		return { assignmentId, userId } as SubmissionDto;
+	};
+
+	it("Counts number of submitted assignments by user", () => {
+		const userIds = ["max", "moritz", "harry", "ronald"];
+
+		const submissions: SubmissionDto[] = [
+			submission("homework-01", "max"),
+			submission("homework-01", "max"),
+			submission("homework-01", "moritz"),
+			submission("testat-01", "max"),
+			submission("testat-01", "moritz"),
+			submission("homework-01", "harry")
+		];
+
+		const countMap = countSubmittedAssignment(submissions, userIds);
+
+		expect(countMap.get("max")).toEqual(2); // Multiple submissions for same assignment are counted as 1
+		expect(countMap.get("moritz")).toEqual(2);
+		expect(countMap.get("harry")).toEqual(1);
+		expect(countMap.get("ronald")).toEqual(0);
+	});
+});
+
+describe("sortStudentsByActivity", () => {
+	const student = (name: string): ParticipantDto => {
+		return { userId: name } as ParticipantDto;
+	};
+
+	it("Prefers higher submission count", () => {
+		const studentsInInvalidGroups: ParticipantDto[] = [
+			student("max"),
+			student("moritz"),
+			student("harry"),
+			student("ronald")
+		];
+
+		const submittedAssignmentsByStudent = new Map([
+			["max", 0], // 4
+			["moritz", 10], // 1
+			["harry", 1], // 3
+			["ronald", 5] // 2
+		]);
+
+		const activeWeekCountByStudent = new Map([
+			["max", 10],
+			["moritz", 0],
+			["harry", 5],
+			["ronald", 2]
+		]);
+
+		const rankedStudents = sortStudentsByActivity(
+			studentsInInvalidGroups,
+			submittedAssignmentsByStudent,
+			activeWeekCountByStudent
+		);
+
+		expect(rankedStudents).toEqual([
+			{ userId: "moritz" },
+			{ userId: "ronald" },
+			{ userId: "harry" },
+			{ userId: "max" }
+		]);
+	});
+
+	it("Equal submission count -> ranks by activity", () => {
+		const studentsInInvalidGroups: ParticipantDto[] = [
+			student("max"),
+			student("moritz"),
+			student("harry")
+		];
+
+		const submittedAssignmentsByStudent = new Map([
+			["max", 10],
+			["moritz", 10],
+			["moritz", 10]
+		]);
+
+		const activeWeekCountByStudent = new Map([
+			["max", 5], // 2
+			["moritz", 10], // 1
+			["harry", 2] // 3
+		]);
+
+		const rankedStudents = sortStudentsByActivity(
+			studentsInInvalidGroups,
+			submittedAssignmentsByStudent,
+			activeWeekCountByStudent
+		);
+
+		expect(rankedStudents).toEqual([
+			{ userId: "moritz" },
+			{ userId: "max" },
+			{ userId: "harry" }
+		]);
+	});
+});
